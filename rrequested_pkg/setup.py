c93c96d47cb168628fca21f5580fc36c1e352813
Astra Bertelli <133636879+AstraBert@users.noreply.github.com>
2023-12-23 20:21:47 +0100

    Create setup.py

---
+++ rrequested_pkg/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rrequested_pkg',
+    version='1.2.0',
+    description='RREQUESTED: A versatile package to preprocess raw basecalled reads with quality and size filtering and ex-novo demultiplexing',
+    author='Astra Bertelli',
+    author_email='astra.bertelli01@universitadipavia.it',
+    url='https://github.com/AstraBert/RREQUESTED',
+    packages=find_packages(),
+    install_requires=[
+        'biopython==1.81',
+        'edlib==1.3.9',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Bioinformaticians',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',    
+    ],
+    long_description=open('readme.md').read(),
+    long_description_content_type='text/markdown',
+)
